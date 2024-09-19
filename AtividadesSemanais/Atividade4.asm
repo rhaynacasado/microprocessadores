13592447543565703c665eb437c913232a2a4b59
rhayna <rhayna.casado@wr.sc.usp.br>
2024-09-19 20:53:55 -0300

    ativ4

--- AtividadesSemanais/Atividade4.asm
+++ AtividadesSemanais/Atividade4.asm
@@ -1,7 +1,9 @@
+; Atividade 1
+
 org 0000h
 
 main:
-	JMP inst_logic_bool
+	JMP transf_dados
 
 transf_dados:
 	MOV A, #12h
@@ -14,6 +16,7 @@ transf_dados:
 	MOV R1, #00110010b
 	MOV A, @R1
 	MOV DPTR, #0x9A5B
+	JMP encerramento
 
 inst_aritm:
 	MOV ACC, #2
@@ -31,8 +34,64 @@ inst_aritm:
 	DIV AB
 	MOV 0x70, A
 	MOV 0x71, B
+	JMP encerramento
 	
 inst_logic_bool:
 	MOV A, #11001100b
 	MOV B, #10101010b
 	ANL A, B
+	RR A
+	RR A
+	CPL A
+	RL A
+	RL A
+	ORL A, B
+	XRL A, B
+	SWAP A
+	JMP encerramento
+
+inst_desvio:
+	CLR A
+	MOV R0, #10h
+	JZ bloco2
+	JNZ bloco3
+	NOP
+
+bloco2:
+	MOV A, R0
+	JMP bloco3 ; Conferir
+
+bloco3:
+	DJNZ R0, bloco3
+	JMP inst_desvio
+
+encerramento:
+	JMP $
+	END
+
+
+
+
+
+; ---------------------
+; Atividade 2
+
+ORG 0000h
+JMP main
+
+main:
+	; ORG 33h ; resolver
+	MOV R0, #20h
+	MOV R1, #0h
+
+loop:
+	MOV A, @R0
+	SUBB A, #45h
+	JNC terceira
+	INC R1
+
+terceira:
+	INC R0
+	CJNE R0, #24h, loop
+	NOP
+	JMP $
