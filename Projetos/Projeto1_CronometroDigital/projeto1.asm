2e462f3c7f16a1e5ac61293f1537658f068d99bc
rhayna <rhaynacasado@usp.br>
2024-10-05 17:41:42 -0300

    projeto1

--- Projetos/Projeto1_CronometroDigital/projeto1.asm
+++ Projetos/Projeto1_CronometroDigital/projeto1.asm
@@ -1,55 +1,67 @@
-; SW0 -> delay de 250ms
-; SW1 -> delay 1000ms
-
 ; Só roda quando SWx estiver pressionado
 
     org 0000h      
 
 main:
-	JNB P2.0, SW0	; Utilizando a P2.0 como SWO e pulando para a label respectiva quando ativada
-;	JNB P2.1, SW1	; Utilizando a P2.1 como SW1 e pulando para a label respectiva quando ativada
-	JMP main
+	JNB P2.0, contagem	; utilizando a P2.0 como SWO e pulando para a label respectiva quando ativada
+	JNB P2.1, contagem	; utilizando a P2.1 como SW1 e pulando para a label respectiva quando ativada
+	JMP desligar		; pula para a label que desliga o display
 
-delay250:			; Rotina para delay de 250 milisegundos
+
+;*******sub-rotina de delay*********
+
+delay:						; salta para a rotina de delay com tempo respectivo a cada switch
+	JNB P2.0, delayA250		
+	JNB P2.1, delayA1000
+	RET						; se nao tiverem switches atividados, retorna para desligar o display
+	
+delayA250: 			; rotina para delay de 250 milisegundos
+	MOV R2, #5		; Utilizando mais um registrador
+delayB250:		 
     MOV R1, #100    
+delayC250:
+    MOV R0, #250   
+    DJNZ R0, $    
+    DJNZ R1, delayC250
+	DJNZ R2, delayB250
+	RET
 
-inicio250:
-    MOV R0, #250    
-    DJNZ R0, $     
-    DJNZ R1, inicio250 
-    RET
+delayA1000: 		; rotina para delay de 1000 milisegundos
+	MOV R2, #20
+delayB1000:		
+    MOV R1, #100    
+delayC1000:
+    MOV R0, #250   
+    DJNZ R0, $    
+    DJNZ R1, delayC1000
+	DJNZ R2, delayB1000
+	RET
 
+;*******ativando display de 7 segmentos*********
 
-delay1000:			; Rotina para delay de 1000 milisegundos (1s)
-    MOV R1, #1000    
+contagem:
+	MOV P1, #11000000b  ; atribui 0 para o display
+	ACALL delay			; chama sub-rotina de delay
+	MOV P1, #11111001b  ; atribui 1 para o display
+	ACALL delay			; chama sub-rotina de delay
+	MOV P1, #10100100b	; atribui 2 para o display
+	ACALL delay			; chama sub-rotina de delay
+	MOV P1, #10110000b	; atribui 3 para o display
+	ACALL delay			; chama sub-rotina de delay
+	MOV P1, #10011001b	; atribui 4 para o display
+	ACALL delay			; chama sub-rotina de delay
+	MOV P1, #10010010b	; atribui 5 para o display
+	ACALL delay			; chama sub-rotina de delay
+	MOV P1, #10000010b	; atribui 6 para o display
+	ACALL delay			; chama sub-rotina de delay
+	MOV P1, #11111000b	; atribui 7 para o display
+	ACALL delay			; chama sub-rotina de delay
+	MOV P1, #10000000b	; atribui 8 para o display
+	ACALL delay			; chama sub-rotina de delay
+	MOV P1, #10011000b	; atribui 9 para o display
+	ACALL delay			; chama sub-rotina de delay
+	JMP main			; retorna para main
 
-inicio1000:
-    MOV R0, #250    
-    DJNZ R0, $     
-    DJNZ R1, inicio1000 
-    RET
-
-
-SW0:
-	MOV P1, #11000000b  ;0
-	ACALL delay250
-	MOV P1, #11111001b  ;1
-	ACALL delay250
-	MOV P1, #10100100b	;2
-	ACALL delay250
-	MOV P1, #10110000b	;3
-	ACALL delay250
-	MOV P1, #10011001b	;4
-	ACALL delay250
-	MOV P1, #10010010b	;5
-	ACALL delay250
-	MOV P1, #10000010b	;6
-	ACALL delay250
-	MOV P1, #11111000b	;7
-	ACALL delay250
-	MOV P1, #10000000b	;8
-	ACALL delay250
-	MOV P1, #10011000b	;9
-	ACALL delay250
-	JMP main
-	 
+desligar:
+	MOV P1, #11111111b  ; desligado
+	JMP main			; retorna para main
