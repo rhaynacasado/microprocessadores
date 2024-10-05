27cee1c34ec1307430b3aecffcd6da83b9d962fd
marceloreginato <mareginato31@gmail.com>
2024-10-05 16:13:30 -0300

    numeros display

--- Projetos/Projeto1_CronometroDigital/projeto1.asm
+++ Projetos/Projeto1_CronometroDigital/projeto1.asm
@@ -6,11 +6,11 @@
     org 0000h      
 
 main:
-	JNB P2.0, sw0
-;	JNB P2.1, sw1
+	JNB P2.0, SW0	; Utilizando a P2.0 como SWO e pulando para a label respectiva quando ativada
+;	JNB P2.1, SW1	; Utilizando a P2.1 como SW1 e pulando para a label respectiva quando ativada
 	JMP main
 
-delay250:
+delay250:			; Rotina para delay de 250 milisegundos
     MOV R1, #100    
 
 inicio250:
@@ -20,7 +20,7 @@ inicio250:
     RET
 
 
-delay1000:
+delay1000:			; Rotina para delay de 1000 milisegundos (1s)
     MOV R1, #1000    
 
 inicio1000:
@@ -28,28 +28,28 @@ inicio1000:
     DJNZ R0, $     
     DJNZ R1, inicio1000 
     RET
-       
 
-sw0:
-	MOV P1, #11000000b ;0
+
+SW0:
+	MOV P1, #11000000b  ;0
 	ACALL delay250
-	MOV P1, #11111001b ;1
+	MOV P1, #11111001b  ;1
 	ACALL delay250
 	MOV P1, #10100100b	;2
 	ACALL delay250
-	MOV P1, #10100100b	;3
+	MOV P1, #10110000b	;3
 	ACALL delay250
-	MOV P1, #10100100b	;4
+	MOV P1, #10011001b	;4
 	ACALL delay250
-	MOV P1, #10100100b	;5
+	MOV P1, #10010010b	;5
 	ACALL delay250
-	MOV P1, #10100100b	;6
+	MOV P1, #10000010b	;6
 	ACALL delay250
-	MOV P1, #10100100b	;7
+	MOV P1, #11111000b	;7
 	ACALL delay250
-	MOV P1, #10100100b	;8
+	MOV P1, #10000000b	;8
 	ACALL delay250
-	MOV P1, #10100100b	;9
+	MOV P1, #10011000b	;9
 	ACALL delay250
 	JMP main
 	 
